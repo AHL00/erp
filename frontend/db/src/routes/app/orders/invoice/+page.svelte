d481036135396ef76b977be9b16c789f5774fc38
Khant Htet Aung <khanthtetaung.khanthtet@gmail.com>
2024-12-08 16:04:09 +0630

    Fixed bank account justify

--- frontend/db/src/routes/app/orders/invoice/+page.svelte
+++ frontend/db/src/routes/app/orders/invoice/+page.svelte
@@ -346,7 +346,7 @@
 
 			<div class="px-8 pb-8 h-fit w-full bottom-0 mt-auto absolute break-inside-avoid">
 				<hr />
-				<div class="flex flex-row justify-end mt-4 space-x-4">
+				<div class="flex flex-row mt-4 space-x-4 justify-between">
 					{#if order?.notes && order?.notes.length > 0}
 						<div class="flex flex-col space-y-1 flex-initial w-1/4">
 							<span class="text-sm text-zinc-800 font-sans font-bold">Notes</span>
@@ -355,14 +355,18 @@
 							>
 						</div>
 					{/if}
-                    {#if business_bank_accounts && business_bank_accounts.length > 0}
-					<div class="flex flex-wrap flex-row place-content-start flex-grow">
-						<span class="w-full h-fit text-sm text-zinc-800 font-sans font-bold mb-1">Bank Accounts</span>
-						{#each business_bank_accounts as account}
-							<span class="w-fit h-fit mr-2 text-xs text-black font-sans font-light">{account}</span>
-						{/each}
-					</div>
-				{/if}
+					{#if business_bank_accounts && business_bank_accounts.length > 0}
+						<div class="flex flex-wrap flex-row place-content-start flex-grow">
+							<span class="w-full h-fit text-sm text-zinc-800 font-sans font-bold mb-1"
+								>Bank Accounts</span
+							>
+							{#each business_bank_accounts as account}
+								<span class="w-fit h-fit mr-2 text-xs text-black font-sans font-light"
+									>{account}</span
+								>
+							{/each}
+						</div>
+					{/if}
 					<div class="flex flex-col items-end space-y-1 flex-initial w-1/4">
 						<span class="text-sm text-zinc-800 font-sans font-bold">Total</span>
 						<CurrencySpan custom_class="text-2xl text-black font-sans font-light" value={total} />
